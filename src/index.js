c65185147753bbd824d9f58b446c6f0a05a57048
Neilketchum <daipayanh@gmail.com>
2020-05-09 00:51:53 +0530

    Optimized and added new routes

--- src/index.js
+++ src/index.js
@@ -7,42 +7,82 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users',async (req, res) => {
     const user = new User(req.body)
-    user.save().then(() => {
-        res.send(user)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
-})
-app.get('/users',(req,res)=>{
-    User.find({}).then((users)=>{
-        res.send(users)
-    }).catch((e)=>{
-        res.status(501).send(e)
-    })
-})
-app.get('/users/:id',(req,res)=>{
-    const _id = req.params.id
-    User.findById(_id).then((user)=>{
-        if(!user){
-            return res.status(404).send()
-        }
+    try{
+        await user.save()
         res.status(200).send(user)
-    }).catch((e)=>{
-        res.status(500).send()
-    })
-})
-app.post('/task',(req,res)=>{
-    const task = new Task(req.body)
-    task.save().then(()=>
-        {
-            res.send(task)
-        }
-    ).catch((e)=>{
+    }catch(e){
         res.status(400).send(e)
-    })
+    }
 })
+app.get('/users',async (req,res)=>{
+    try{
+        const response  = await User.find({})
+        res.status(200).send(response)
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+app.get('/users/:id',async (req,res)=>{
+    const _id = req.params.id
+    try{
+        const user = await User.findById((_id))
+        res.status(200).send(user)
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
+app.patch('/users/:id',async (req,res)=>{
+    const allowedUpdates = ['name','age','email','password']
+    const user_id = req.params.id
+    const updates = Object.keys(req.body)
+    const isValid = updates.every((update)=>{
+        return allowedUpdates.includes(update)
+    })
+    if(!isValid){
+        return res.status(400).send("Invaild Updates")
+    }else{
+        try {
+            const user = await User.findByIdAndUpdate(user_id,req.body,{
+                new : true,
+                runValidators:true
+            })
+            if(!user){
+                return res.status(404).send("No Data")
+            }
+            res.send(user)
+        } catch (error) {
+            res.status(501).send(error)
+        }
+    }
+})
+app.post('/task',async (req,res)=>{
+    const task = new Task(req.body)
+    try {
+        await res.send(task)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+app.get('/task',async (req,res)=>{
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+app.get('/task/:id',async (req,res)=>{
+    const task_id = req.params.id
+    try {
+        const tasks = await Task.findById((task_id))
+        res.send(tasks)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
